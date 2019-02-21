de8fb5b813fe634aa2fbb1dcaef279b95b73f54c
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-21 16:55:39 +0000

    refactor: grid no longer an instance variable

--- lib/place_marker.rb
+++ lib/place_marker.rb
@@ -2,27 +2,25 @@ class PlaceMarker
   class InvalidMoveError < RuntimeError
   end
 
-  attr_reader :grid
-
   def initialize(game_gateway)
     @game_gateway = game_gateway
   end
 
   def execute(marker, position)
-    @grid = @game_gateway.saved_game.grid
+    grid = @game_gateway.saved_game.grid
     return if position.nil?
 
-    raise InvalidMoveError if already_taken?(position)
+    raise InvalidMoveError if already_taken?(grid, position)
 
-    @grid[position[0]][position[1]] = marker
+    grid[position[0]][position[1]] = marker
 
-    game = Game.new(@grid)
+    game = Game.new(grid)
     @game_gateway.save(game)
   end
 
   private
 
-  def already_taken?(position)
-    !@grid[position[0]][position[1]].nil?
+  def already_taken?(grid, position)
+    !grid[position[0]][position[1]].nil?
   end
 end

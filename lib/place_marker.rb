669362e33d42975f4c3582f5e1add785e0b2bdab
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-21 16:09:11 +0000

    refactor: move marker as parameter of #execute

--- lib/place_marker.rb
+++ lib/place_marker.rb
@@ -4,18 +4,17 @@ class PlaceMarker
 
   attr_reader :grid
 
-  def initialize(game_gateway, marker)
+  def initialize(game_gateway)
     @game_gateway = game_gateway
-    @marker = marker
   end
 
-  def execute(position)
+  def execute(marker, position)
     @grid = @game_gateway.saved_game.grid
     return if position.nil?
 
     raise InvalidMoveError if already_taken?(position)
 
-    @grid[position[0]][position[1]] = @marker
+    @grid[position[0]][position[1]] = marker
 
     game = Game.new(@grid)
     @game_gateway.save(game)

b53d444d0aff97a8d3e127ea61425445e8d8bd83
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-21 16:10:07 +0000

    test: refactor for marker parameter change

--- spec/unit/place_marker_spec.rb
+++ spec/unit/place_marker_spec.rb
@@ -11,8 +11,8 @@ describe PlaceMarker do
   end
 
   let(:game_gateway) { GameGatewayStubAndSpy.new }
-  let(:place_x_marker) { PlaceMarker.new(game_gateway, :x) }
-  let(:place_o_marker) { PlaceMarker.new(game_gateway, :o) }
+  let(:place_x_marker) { PlaceMarker.new(game_gateway) }
+  let(:place_o_marker) { PlaceMarker.new(game_gateway) }
   let(:empty_grid) do
     [
       [nil, nil, nil],
@@ -25,7 +25,7 @@ describe PlaceMarker do
 
   context 'place X marker' do
     it 'can place X marker at (2, 1) on the grid' do
-      place_x_marker.execute([2, 1])
+      place_x_marker.execute(:x, [2, 1])
 
       expect(game_gateway.saved_game.grid).to eq(
         [
@@ -39,7 +39,7 @@ describe PlaceMarker do
 
   context 'place O marker' do
     it 'can place O marker at (0, 0) on the grid' do
-      place_o_marker.execute([0, 0])
+      place_o_marker.execute(:o, [0, 0])
 
       expect(game_gateway.saved_game.grid).to eq(
         [
@@ -51,7 +51,7 @@ describe PlaceMarker do
     end
 
     it 'can place O marker at (1, 2) on the grid' do
-      place_o_marker.execute([1, 2])
+      place_o_marker.execute(:o, [1, 2])
 
       expect(game_gateway.saved_game.grid).to eq(
         [
@@ -65,23 +65,23 @@ describe PlaceMarker do
 
   context 'raise error' do
     it 'can raise an error when a O marker is placed at (2, 1) on a taken position' do
-      place_x_marker.execute([2, 1])
-      expect { place_o_marker.execute([2, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      place_x_marker.execute(:o, [2, 1])
+      expect { place_o_marker.execute(:x, [2, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
     end
 
     it 'can raise an error when a O marker is placed at (2, 2) on a taken position' do
-      place_x_marker.execute([2, 2])
-      expect { place_o_marker.execute([2, 2]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      place_x_marker.execute(:o, [2, 2])
+      expect { place_o_marker.execute(:x, [2, 2]) }.to raise_error(PlaceMarker::InvalidMoveError)
     end
 
     it 'can raise an error when an X marker is placed at (0, 1) on a taken position' do
-      place_o_marker.execute([0, 1])
-      expect { place_x_marker.execute([0, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      place_o_marker.execute(:x, [0, 1])
+      expect { place_x_marker.execute(:o, [0, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
     end
 
     it 'can raise an error when an X marker is placed at (1, 1) on a taken position' do
-      place_o_marker.execute([1, 1])
-      expect { place_x_marker.execute([1, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      place_o_marker.execute(:x, [1, 1])
+      expect { place_x_marker.execute(:o, [1, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
     end
   end
 end

58f9e14674c84f2d2d54f8ab7c9c007001296b3c
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-25 11:12:17 +0000

    feat: check for win for X and O

--- lib/win_horizontal_game.rb
+++ lib/win_horizontal_game.rb
@@ -2,8 +2,8 @@ class WinHorizontalGame
   def initialize(game_gateway)
     @game_gateway = game_gateway
   end
-  
+
   def execute
-    'Player X has won the game!'
+    @game_gateway.saved_game.horizontal_win
   end
 end

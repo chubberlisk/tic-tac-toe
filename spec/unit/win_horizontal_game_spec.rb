3c2963b5abdb66d5d3ea77a17924ade99145744f
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-25 11:12:01 +0000

    test: add more unit tests for X and O horizontal wins

--- spec/unit/win_horizontal_game_spec.rb
+++ spec/unit/win_horizontal_game_spec.rb
@@ -3,19 +3,90 @@ require_relative '../../lib/game'
 
 describe WinHorizontalGame do
   class GameGatewayStub
-    attr_writer :saved_game
+    attr_accessor :saved_game
   end
 
-  it 'can win a game when player X has 3 in a row horizontally in the first row' do 
-    game_gateway = GameGatewayStub.new
-    win_horizontal_game = WinHorizontalGame.new(game_gateway)
+  let(:game_gateway) { GameGatewayStub.new }
+  let(:win_horizontal_game) { WinHorizontalGame.new(game_gateway) }
+
+  context 'when Player X wins' do
+    it 'can win a game when player X has 3 in a row horizontally in the first row' do 
+      game_gateway.saved_game = Game.new(
+        [
+          [:x, :x, :x],
+          [:o, :o, nil],
+          [nil, nil, nil]
+        ]
+      )
+      expect(win_horizontal_game.execute).to eq('Player X has won the game!')
+    end
+
+    it 'can win a game when player X has 3 in a row horizontally in the second row' do 
+      game_gateway.saved_game = Game.new(
+        [
+          [:o, :o, nil],
+          [:x, :x, :x],
+          [nil, nil, nil]
+        ]
+      )
+      expect(win_horizontal_game.execute).to eq('Player X has won the game!')
+    end
+
+    it 'can win a game when player X has 3 in a row horizontally in the last row' do 
+      game_gateway.saved_game = Game.new(
+        [
+          [:o, :o, nil],
+          [nil, nil, nil],
+          [:x, :x, :x]
+        ]
+      )
+      expect(win_horizontal_game.execute).to eq('Player X has won the game!')
+    end
+  end
+
+  context 'when Player O wins' do
+    it 'can win a game when player O has 3 in a row horizontally in the first row' do 
+      game_gateway.saved_game = Game.new(
+        [
+          [:o, :o, :o],
+          [:x, :x, nil],
+          [nil, nil, nil]
+        ]
+      )
+      expect(win_horizontal_game.execute).to eq('Player O has won the game!')
+    end
+
+    it 'can win a game when player O has 3 in a row horizontally in the second row' do 
+      game_gateway.saved_game = Game.new(
+        [
+          [:x, :x, nil],
+          [:o, :o, :o],
+          [nil, nil, nil]
+        ]
+      )
+      expect(win_horizontal_game.execute).to eq('Player O has won the game!')
+    end
+
+    it 'can win a game when player O has 3 in a row horizontally in the third row' do 
+      game_gateway.saved_game = Game.new(
+        [
+          [:x, :x, nil],
+          [nil, nil, nil],
+          [:o, :o, :o]
+        ]
+      )
+      expect(win_horizontal_game.execute).to eq('Player O has won the game!')
+    end
+  end
+
+  it 'can recognise when there is no horizontal win' do
     game_gateway.saved_game = Game.new(
       [
-        [:x, :x, :x],
+        [:x, :o, :x],
         [:o, :o, nil],
         [nil, nil, nil]
       ]
     )
-    expect(win_horizontal_game.execute).to eq('Player X has won the game!')
+    expect(win_horizontal_game.execute).to eq('No horizontal win.')
   end
 end

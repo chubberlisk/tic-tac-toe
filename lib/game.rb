1297a6a8a975cb9d0399d173cc825ac68eca56e0
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-25 11:12:33 +0000

    feat: check for horizontal win for X and O

--- lib/game.rb
+++ lib/game.rb
@@ -4,4 +4,26 @@ class Game
   def initialize(grid)
     @grid = grid
   end
+
+  def horizontal_win
+    if winner
+      "Player #{winner} has won the game!"
+    else
+      'No horizontal win.'
+    end
+  end
+
+  private
+
+  def winner
+    if win?(:x)
+      'X'
+    elsif win?(:o)
+      'O'
+    end
+  end
+
+  def win?(marker)
+    @grid[0][0..2].all?(marker) || @grid[1][0..2].all?(marker) || @grid[2][0..2].all?(marker)
+  end
 end

08ac10e09b8792e4dd0391d65ea94c996a0c2dd0
Wen Ting Wang <chubberlisk@outlook.com>
2019-02-26 10:26:49 +0000

    feat: check for vertical win

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ class Game
     @grid = grid
   end
 
-  def horizontal_win
+  def win
     return :player_x_win if win?(:x)
     return :player_o_win if win?(:o)
 
@@ -15,7 +15,20 @@ class Game
   private
 
   def win?(marker)
+    horizontal_win?(marker) || vertical_win?(marker)
+  end
+
+  def horizontal_win?(marker)
     @grid[0][0..2].all?(marker) || @grid[1][0..2].all?(marker)\
-      || @grid[2][0..2].all?(marker)
+    || @grid[2][0..2].all?(marker)
+  end
+
+  def vertical_win?(marker)
+    grid_column(0).all?(marker) || grid_column(1).all?(marker)\
+    || grid_column(2).all?(marker)
+  end
+
+  def grid_column(index)
+    [@grid[0][index], @grid[1][index], @grid[2][index]]
   end
 end

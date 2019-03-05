c9ece9c78adc45d192713cf9bf6bd981dda6e98d
Maysa <maysa@madetech.com>
2019-03-05 15:56:42 +0000

    test: refactor test description

--- spec/unit/create_new_grid_spec.rb
+++ spec/unit/create_new_grid_spec.rb
@@ -2,7 +2,7 @@ require_relative '../test_doubles/grid_gateway_spy'
 require_relative '../../lib/create_new_grid'
 
 describe CreateNewGrid do
-  it 'saves a game' do
+  it 'creates a new grid' do
     grid_gateway = GridGatewaySpy.new
     start_new_game = CreateNewGrid.new(grid_gateway)
 

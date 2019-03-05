eac69c338636acb0385c380fec0746570064fb0a
Wen Ting Wang <chubberlisk@outlook.com>
2019-03-05 09:20:57 +0000

    feat: raises invalid turn error if not players turn

--- lib/place_marker.rb
+++ lib/place_marker.rb
@@ -1,6 +1,7 @@
 class PlaceMarker
   InvalidMoveError = Class.new(RuntimeError)
   InvalidPositionError = Class.new(RuntimeError)
+  InvalidTurnError = Class.new(RuntimeError)
 
   def initialize(game_gateway)
     @game_gateway = game_gateway
@@ -10,6 +11,8 @@ class PlaceMarker
     raise InvalidPositionError unless inside_grid?(position)
 
     grid = @game_gateway.saved_game.grid
+
+    raise InvalidTurnError if not_players_turn?(grid, marker)
 
     raise InvalidMoveError if already_taken?(grid, position)
 
@@ -27,5 +30,17 @@ class PlaceMarker
 
   def inside_grid?(position)
     position[0].between?(0, 2) && position[1].between?(0, 2)
+  end
+
+  def not_players_turn?(grid, marker)
+    total_x_count = grid.reduce(0) { |total, row| total + row.count(:x) }
+    total_o_count = grid.reduce(0) { |total, row| total + row.count(:o) }
+    total_x_count > total_o_count && marker == :x
+    total_x_count < total_o_count && marker == :o
+    if marker == :x
+      total_x_count > total_o_count
+    else
+      total_x_count < total_o_count
+    end
   end
 end

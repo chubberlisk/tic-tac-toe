fd3260afd346e9f46440795b1e8b11ee3f76a9b8
Rob McHugh <robert@madetech.com>
2019-03-11 15:38:55 +0000

    Adding error messages for take_turn usecase

--- spec/unit/take_turn_spec.rb
+++ spec/unit/take_turn_spec.rb
@@ -90,4 +90,47 @@ describe UseCase::TakeTurn do
 
     expect(response[:player_turn]).to eq(:player_x)
   end
+
+  it 'returns an error message when a turn is made outside of the grid' do
+    game_options = {
+      first_player: :player_o
+    }
+
+    game = Game.new(game_options)
+
+    game_gateway = GameGatewayFake.new
+    game_gateway.saved_game = game
+
+    take_turn = UseCase::TakeTurn.new(game_gateway)
+
+    turn_options = {
+      position: [100, 0]
+    }
+
+    response = take_turn.execute(turn_options)
+
+    expect(response[:error]).to eq("Please place your marker inside the grid")
+  end
+
+  it 'returns an error message when a turn is made on a taken tile' do
+    game_options = {
+      first_player: :player_o
+    }
+
+    game = Game.new(game_options)
+
+    game_gateway = GameGatewayFake.new
+    game_gateway.saved_game = game
+
+    take_turn = UseCase::TakeTurn.new(game_gateway)
+
+    turn_options = {
+      position: [1, 0]
+    }
+
+    response = take_turn.execute(turn_options)
+    response = take_turn.execute(turn_options)
+
+    expect(response[:error]).to eq("That tile is already taken, please select another")
+  end
 end

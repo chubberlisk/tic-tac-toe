e4c84cc8a61b4ff5391054bbd79435e314facc5b
Wen Ting Wang <chubberlisk@outlook.com>
2019-03-11 15:47:33 +0000

    Refactor unit tests for place marker

--- spec/unit/place_marker_spec.rb
+++ spec/unit/place_marker_spec.rb
@@ -1,8 +1,16 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe PlaceMarker do
-  let(:grid_gateway) { GridGatewayFake.new }
-  let(:place_marker) { PlaceMarker.new(grid_gateway) }
+describe UseCase::PlaceMarker do
+  let(:game_gateway) { GameGatewayFake.new }
+  let(:place_marker) { UseCase::PlaceMarker.new }
+  let(:game) { Game.new }
+  let(:options) do
+    {
+      player_turn: game.player_turn,
+      grid: game.grid,
+      position: [0, 0]
+    }
+  end
   let(:empty_grid) do
     [
       [nil, nil, nil],
@@ -11,110 +19,142 @@ describe PlaceMarker do
     ]
   end
 
-  before { grid_gateway.saved_grid = Grid.new }
+  before { game_gateway.saved_game = game }
 
-  context 'when an X marker is placed' do
-    it 'can place X marker at (2, 1) on the grid' do
-      place_marker.execute(:x, [2, 1])
+  it 'can place X marker at (2, 1) on the grid' do
+    options[:position] = [2, 1]
 
-      expect(grid_gateway.saved_grid.state).to eq(
-        [
-          [nil, nil, nil],
-          [nil, nil, nil],
-          [nil, :x, nil]
-        ]
-      )
-    end
+    place_marker.execute(options)
+
+    expect(game_gateway.saved_game.grid).to eq(
+      [
+        [nil, nil, nil],
+        [nil, nil, nil],
+        [nil, :x, nil]
+      ]
+    )
   end
 
-  context 'when an O marker is placed' do
-    it 'can place O marker at (0, 0) on the grid' do
-      place_marker.execute(:o, [0, 0])
+  it 'can place X marker at (0, 1) on the grid' do
+    options[:position] = [0, 1]
 
-      expect(grid_gateway.saved_grid.state).to eq(
-        [
-          [:o, nil, nil],
-          [nil, nil, nil],
-          [nil, nil, nil]
-        ]
-      )
-    end
+    place_marker.execute(options)
 
-    it 'can place O marker at (1, 2) on the grid' do
-      place_marker.execute(:o, [1, 2])
+    expect(game_gateway.saved_game.grid).to eq(
+      [
+        [nil, :x, nil],
+        [nil, nil, nil],
+        [nil, nil, nil]
+      ]
+    )
+  end
 
-      expect(grid_gateway.saved_grid.state).to eq(
-        [
-          [nil, nil, nil],
-          [nil, nil, :o],
-          [nil, nil, nil]
-        ]
-      )
-    end
+  it 'can place O marker at (0, 0) on the grid' do
+    options[:player_turn] = :player_o
+    options[:position] = [0, 0]
+
+    place_marker.execute(options)
+
+    expect(game_gateway.saved_game.grid).to eq(
+      [
+        [:o, nil, nil],
+        [nil, nil, nil],
+        [nil, nil, nil]
+      ]
+    )
+  end
+
+  it 'can place O marker at (2, 2) on the grid' do
+    options[:player_turn] = :player_o
+    options[:position] = [2, 2]
+
+    place_marker.execute(options)
+
+    expect(game_gateway.saved_game.grid).to eq(
+      [
+        [nil, nil, nil],
+        [nil, nil, nil],
+        [nil, nil, :o]
+      ]
+    )
   end
 
   context 'when marker is placed on a taken position' do
     it 'can raise an error when an X marker is placed at (2, 1) on a taken position' do
-      place_marker.execute(:o, [2, 1])
-      expect { place_marker.execute(:x, [2, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      options[:player_turn] = :player_o
+      options[:position] = [2, 1]
+
+      place_marker.execute(options)
+
+      options[:player_turn] = :player_x
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidMoveError)
     end
 
     it 'can raise an error when an X marker is placed at (2, 2) on a taken position' do
-      place_marker.execute(:o, [2, 2])
-      expect { place_marker.execute(:x, [2, 2]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      options[:player_turn] = :player_o
+      options[:position] = [2, 2]
+
+      place_marker.execute(options)
+
+      options[:player_turn] = :player_x
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidMoveError)
     end
 
     it 'can raise an error when an O marker is placed at (0, 1) on a taken position' do
-      place_marker.execute(:x, [0, 1])
-      expect { place_marker.execute(:o, [0, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      options[:player_turn] = :player_x
+      options[:position] = [0, 1]
+
+      place_marker.execute(options)
+
+      options[:player_turn] = :player_o
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidMoveError)
     end
 
     it 'can raise an error when an O marker is placed at (1, 1) on a taken position' do
-      place_marker.execute(:x, [1, 1])
-      expect { place_marker.execute(:o, [1, 1]) }.to raise_error(PlaceMarker::InvalidMoveError)
+      options[:player_turn] = :player_x
+      options[:position] = [1, 1]
+
+      place_marker.execute(options)
+
+      options[:player_turn] = :player_o
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidMoveError)
     end
   end
 
   context 'when marker is placed outside the grid' do
     it 'can raise an error when an X marker is placed outside of the X coordinate of the grid' do
-      expect { place_marker.execute(:x, [3, 0]) }.to raise_error(PlaceMarker::InvalidPositionError)
+      options[:player_turn] = :player_x
+      options[:position] = [3, 0]
+
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidPositionError)
     end
 
     it 'can raise an error when an X marker is placed outside of the X coordinate of the grid' do
-      expect { place_marker.execute(:x, [6, 1]) }.to raise_error(PlaceMarker::InvalidPositionError)
+      options[:player_turn] = :player_x
+      options[:position] = [6, 1]
+
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidPositionError)
     end
 
     it 'can raise an error when an X marker is placed outside of the X coordinate of the grid' do
-      expect { place_marker.execute(:x, [-1, 2]) }.to raise_error(PlaceMarker::InvalidPositionError)
+      options[:player_turn] = :player_x
+      options[:position] = [-1, 2]
+
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidPositionError)
     end
 
     it 'can raise an error when an X marker is placed outside of the Y coordinate of the grid' do
-      expect { place_marker.execute(:x, [1, 4]) }.to raise_error(PlaceMarker::InvalidPositionError)
+      options[:player_turn] = :player_x
+      options[:position] = [1, 4]
+
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidPositionError)
     end
 
     it 'can raise an error when an X marker is placed outside of the Y coordinate of the grid' do
-      expect { place_marker.execute(:x, [1, -1]) }.to raise_error(PlaceMarker::InvalidPositionError)
-    end
-  end
+      options[:player_turn] = :player_x
+      options[:position] = [1, -1]
 
-  context 'when it is not the turn of the player to place a marker' do
-    it 'can raise an error when the Player X takes two turns in a row' do
-      grid_gateway.saved_grid = Grid.new
-
-      place_marker.execute(:x, [1, 1])
-      place_marker.execute(:o, [0, 0])
-      place_marker.execute(:x, [2, 1])
-
-      expect { place_marker.execute(:x, [1, 2]) }.to raise_error(PlaceMarker::InvalidTurnError)
-    end
-
-    it 'can raise an error when the Player O takes two turns in a row' do
-      grid_gateway.saved_grid = Grid.new
-
-      place_marker.execute(:x, [0, 1])
-      place_marker.execute(:o, [1, 1])
-
-      expect { place_marker.execute(:o, [2, 1]) }.to raise_error(PlaceMarker::InvalidTurnError)
+      expect { place_marker.execute(options) }.to raise_error(UseCase::PlaceMarker::InvalidPositionError)
     end
   end
 end

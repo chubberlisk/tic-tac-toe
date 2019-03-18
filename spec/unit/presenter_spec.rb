1f741235b9790d4102991997e43dbed594b1e7a8
Wen Ting Wang <chubberlisk@outlook.com>
2019-03-18 10:37:15 +0000

    Add tests for taking a turn and end game

--- spec/unit/presenter_spec.rb
+++ spec/unit/presenter_spec.rb
@@ -1,86 +1,120 @@
 describe UI::Presenter do
-
-    class StartNewGameSpy
-      def initialize 
-        @called = false
-      end
-
-      def called?
-        @called
-      end
-
-      def execute(*)
-        @called = true
-      end
+  class StartNewGameSpy
+    def initialize
+      @called = false
     end
 
-    class EvaluateGameStub
-      def execute(*)
-        {outcome: nil}
-      end
-
+    def called?
+      @called
     end
 
-    class TakeTurnStub
+    def execute(*)
+      @called = true
+    end
+  end
 
+  class EvaluateGameStub
+    attr_writer :stop_calls_after
+    attr_reader :number_of_calls
+
+    def initialize
+      @number_of_calls = 0
+      @stop_calls_after = 0
     end
 
-    class UserInterfaceFake
-      def initialize
-        @display_game_screen_called = false
-      end
+    def execute(*)
+      @number_of_calls += 1
 
-      def ask_user_for_first_player
-        :player_x
-      end
-
-      def display_game_screen(*)
-        @display_game_screen_called = true
-      end
-
-      def display_game_screen_called?
-        @display_game_screen_called
-      end
-
-      def display_end_screen(*)
-
+      if @number_of_calls > @stop_calls_after
+        { outcome: nil }
+      else
+        { outcome: :continue }
       end
     end
+  end
 
+  class TakeTurnStub
+    def initialize
+      @called = false
+    end
+
+    def called?
+      @called
+    end
+
+    def execute(*)
+      @called = true
+    end
+  end
+
+  class UserInterfaceFake
+    attr_reader :display_game_screen_called
+
+    def initialize
+      @display_game_screen_called = 0
+    end
+
+    def ask_user_for_first_player
+      :player_x
+    end
+
+    def ask_user_for_position
+    end
+
+    def display_game_screen(*)
+      @display_game_screen_called += 1
+    end
+
+    def display_end_screen(*)
+    end
+  end
+
+  let(:start_new_game_spy) { StartNewGameSpy.new }
+  let(:evaluate_game_stub) { EvaluateGameStub.new }
+  let(:take_turn_stub) { TakeTurnStub.new }
+  let(:user_interface_fake) { UserInterfaceFake.new }
+  let(:presenter) do
+    UI::Presenter.new(
+      start_new_game: start_new_game_spy,
+      evaluate_game: evaluate_game_stub,
+      take_turn: take_turn_stub,
+      user_interface: user_interface_fake
+    )
+  end
 
   it 'can start a new game' do
-    start_new_game_spy = StartNewGameSpy.new
-    evaluate_game_stub = EvaluateGameStub.new
-    take_turn_stub = TakeTurnStub.new
-    user_interface_fake = UserInterfaceFake.new
+    presenter.execute
 
-    presenter = UI::Presenter.new(
-      start_new_game: start_new_game_spy,
-      evaluate_game: evaluate_game_stub, 
-      take_turn: take_turn_stub, 
-      user_interface: user_interface_fake
-      )
-      presenter.execute
-
-      expect(start_new_game_spy.called?).to eq(true)
+    expect(start_new_game_spy.called?).to eq(true)
   end
 
   it 'can display the new game' do
-    start_new_game_spy = StartNewGameSpy.new
-    evaluate_game_stub = EvaluateGameStub.new
-    take_turn_stub = TakeTurnStub.new
-    user_interface_fake = UserInterfaceFake.new
+    presenter.execute
 
-    presenter = UI::Presenter.new(
-      start_new_game: start_new_game_spy,
-      evaluate_game: evaluate_game_stub, 
-      take_turn: take_turn_stub, 
-      user_interface: user_interface_fake
-      )
-      presenter.execute
-
-      expect(user_interface_fake.display_game_screen_called?).to eq(true)
+    expect(user_interface_fake.display_game_screen_called).to eq(1)
   end
 
+  it 'can take a turn' do
+    evaluate_game_stub.stop_calls_after = 1
 
+    presenter.execute
+
+    expect(take_turn_stub.called?).to eq(true)
+  end
+
+  it 'can display the turn' do
+    evaluate_game_stub.stop_calls_after = 1
+
+    presenter.execute
+
+    expect(user_interface_fake.display_game_screen_called).to eq(2)
+  end
+
+  it 'can end the game' do
+    evaluate_game_stub.stop_calls_after = 1
+
+    presenter.execute
+
+    expect(evaluate_game_stub.number_of_calls).to eq(3)
+  end
 end
